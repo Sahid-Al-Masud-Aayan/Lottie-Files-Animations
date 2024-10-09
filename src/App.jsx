e2586c3b8ec8e5a06af9b1214fbe36db45e42945
Sahid-Al-Masud-Aayan <aayan.webdeveloper@gmail.com>
2024-10-09 23:29:37 +0600

    no use of this shit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Lottie from "lottie-react";
+import AK47 from '../public/AK47animated.json'
+import car from '../public/Animation - 1719919744350.json'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Lottie animationData={AK47}/>
+    <Lottie animationData={car}/>
+    </>
+  )
+}
+
+export default App
